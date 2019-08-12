c6315694bffc4c192324c2a5d82273c684b612d1
Ruben Navarro <44641367+navarroruben@users.noreply.github.com>
2019-08-12 10:16:25 -0400

    Update OracleOgres.java

--- OracleOgres.java
+++ OracleOgres.java
@@ -1,8 +1,8 @@
-// Ruben Navarro
-// Date: 03/30/2019
-// COP3252
-// Assignment 5
-// OracleOgres.java
+/**********************/
+// Date: March 30, 2019
+// Name: Ruben Navarro
+// Java-Bean Forest
+/**********************/
 
 public class OracleOgres extends Enemy {
 
